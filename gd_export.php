4a038a3446564d119cdb075c708a2477c7972774
Komiya Yu <komiya.yu@n138.jp>
2024-08-07 16:47:16 +0900

    webp

--- gd_export.php
+++ gd_export.php
@@ -2,7 +2,7 @@
 session_start();
 date_default_timezone_set('Asia/Tokyo');
 header('content-type: text/plain');
-header('content-type: image/png');
+header('content-type: image/webp');
 
 // 空の画像を作成し、テキストを追加します
 $im = imagecreatetruecolor(520, 520);
@@ -10,7 +10,7 @@ $fillcolor = imagecolorallocate($im, 240, 248, 255);
 imagefill($im, 0, 0, $fillcolor);
 
 // 画像を出力します
-imagepng($im);
+imagewebp($im);
 
 // メモリを開放します
 imagedestroy($im);
